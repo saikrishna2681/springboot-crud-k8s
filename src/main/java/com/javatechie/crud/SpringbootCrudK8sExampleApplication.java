dd2094560931834b9c99fa5aa1269a4d613b94cb
simple.sai220@gmail.com <simple.sai220@gmail.com>
2023-09-27 09:04:48 +0530

    spring crud app

--- src/main/java/com/javatechie/crud/SpringbootCrudK8sExampleApplication.java
+++ src/main/java/com/javatechie/crud/SpringbootCrudK8sExampleApplication.java
@@ -1,13 +1,18 @@
 package com.javatechie.crud;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringbootCrudK8sExampleApplication {
 
+	private static final Logger logger = LoggerFactory.getLogger(SpringbootCrudK8sExampleApplication.class);
 	public static void main(String[] args) {
+		logger.info("this is a info message");
 		SpringApplication.run(SpringbootCrudK8sExampleApplication.class, args);
+		logger.info("App execution completed");
 	}
 
 }
