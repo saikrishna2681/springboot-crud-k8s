d4f5fa5cef2bcfa9b8da6a7266703b3f28acf572
saikrishna <45065762+saikrishna2681@users.noreply.github.com>
2024-07-14 13:32:06 +0530

    Update backup_log.sh

--- backup_log.sh
+++ backup_log.sh
@@ -1,6 +1,5 @@
 apt-get update -y
 apt-get install zip -y
 apt-get install awscli
-aws configure set aws_access_key_id "AKIA5FTY6YB3XG7DJ46H"; aws configure set aws_secret_access_key "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"; aws configure set default.region "ap-south-1"
 zip -j log.zip /var/lib/springapp/*
 aws s3 cp log.zip s3://log-info2681
